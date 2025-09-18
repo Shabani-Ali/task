e89859714160a3453887f5de483ecc30e2e3b694
Shabani Ali <alimshabani22@gmail.com>
2025-09-18 12:17:12 +0200

    Add routes for tasks and projects

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function(){return redirect()->route('tasks.index');});
+Route::get('/tasks',[TaskController::class, 'index'])->name('tasks.index');
+Route::post('/tasks',[TaskController::class, 'store'])->name('tasks.store');
+Route::get('/tasks/{task}/edit',[TaskController::class, 'edit'])->name('tasks.edit');
+Route::get('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::post('/tasks/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
+
+Route::resource('projects',ProjectController::class)->except(['show']);
